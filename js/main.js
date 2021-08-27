e04004bdaf681200346f04c27996a0f5893adf1d
Eduardo Lacerda <eduardo.filho@bistowbis.com.br>
2021-08-27 14:54:37 -0300

    Atualização de layout

--- js/main.js
+++ js/main.js
@@ -12,16 +12,35 @@ var carousel = new Carousel({
     btnNext: '.next'
 });
 
-btn.addEventListener('click', function () {
+var nav = document.querySelector('.top-nav');
+var mask = document.querySelector('.mask');
+var btnBurger = document.querySelector('.burger-button');
+
+btnBurger.addEventListener('click', function() {
+    nav.toggleAttribute('opened');
+    mask.toggleAttribute('opened');
+    clickOut();
+});
+
+function clickOut() {
+    window.addEventListener('click', function(e) {
+        if (!nav.contains(e.target.parentNode) && e.target != btnBurger) {
+            nav.removeAttribute('opened');
+            mask.removeAttribute('opened');
+        }
+    });
+}
+
+btn.addEventListener('click', function() {
     // Fonte do Regex: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
     // const e = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    
+
     const e = /[a-z0-9.]*@[a-z0-9]*\.[a-z\.]*/
     const isValid = e.test(email.value);
 
     if (!isValid) {
         email.setCustomValidity('');
-            
+
         if (email.validity.valueMissing) {
             email.setCustomValidity('Digite seu e-mail aqui');
         } else {
